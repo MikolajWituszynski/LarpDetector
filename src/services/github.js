af84a95f5bca268ed711c4c4dd4acb0a7cca8cd9
meekwit <74858186+MikolajWituszynski@users.noreply.github.com>
2025-01-03 20:03:58 +0100

    Update github.js

--- src/services/github.js
+++ src/services/github.js
@@ -18,7 +18,7 @@ const fetchGitHubAPI = async (endpoint) => {
       headers: {
         'Accept': 'application/vnd.github.v3+json',
         'User-Agent': 'GitHub-Health-Check',
-      'Authorization': `Bearer ${githubToken}`
+      'Authorization': `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`
       }
     });
     
